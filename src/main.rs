2362ed61a47bee9aa69a4340356c75da8f2dc94f
Ju, Seonghwan <crtevn@gmail.com>
2025-03-17 20:41:43 +0900

    Warp로 Http 서버 만들어 보기
    localhost:8080/ 경로로 Hello Function World! 띄워보자

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,24 @@ mod exercises {
     }
 }
 
-fn main() {
+use warp::Filter;
 
+#[tokio::main]
+async fn main() {
+    // HTML 페이지 정의
+    let html_page = r#"
+    <html>
+        <body>
+            <h1 style="text-align:center; font-size:3em";>Hello Function World!</h1>
+        </body>
+    </html>
+    "#;
+
+    let handler = warp::path::end()
+        .map(move || warp::reply::html(html_page));
+
+    warp::serve(handler)
+        .run(([127, 0, 0, 1], 8080))
+        .await;
 }
+
